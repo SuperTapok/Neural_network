67df5b753fb5851f26b8824896b7ac522c353a2b
SuperTapok <noname20041@mail.ru>
2020-11-15 22:04:22 +0700

    Create image.py

---
+++ image.py
@@ -0,0 +1,28 @@
+import numpy as np
+from PIL import Image
+
+im = Image.open('buldogek.jpg')  # Can be many different formats.
+im2arr = np.array(im)
+
+for h in range(im2arr.shape[0]):
+    for w in range(im2arr.shape[1]):
+        for c in range(im2arr.shape[2]):
+            if c == 0:
+                tmp = round(im2arr[h, w, c] + 75)
+                tmp = tmp if tmp <= 255 else 255
+                tmp = tmp if tmp >= 0 else 0
+                im2arr[h, w, c] = tmp
+            # if c == 1:
+            #     tmp = round(im2arr[h, w, c] + 75)
+            #     tmp = tmp if tmp <= 255 else 255
+            #     tmp = tmp if tmp >= 0 else 0
+            #     im2arr[h, w, c] = tmp
+            #  if c == 2:
+            #      tmp = round(im2arr[h, w, c] + 75)
+            #      tmp = tmp if tmp <= 255 else 255
+            #      tmp = tmp if tmp >= 0 else 0
+            #      im2arr[h, w, c] = tmp
+
+img = Image.fromarray(im2arr, 'RGB')
+img.save('buldogek.png')
+img.show()
