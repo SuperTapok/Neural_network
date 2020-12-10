f1690383bc9a6cfd46faf6bf2577598787996de8
SuperTapok <56495958+SuperTapok@users.noreply.github.com>
2020-12-10 23:51:24 +0700

    Create OOP.py

---
+++ OOP.py
@@ -0,0 +1,12 @@
+class ClassExample():
+    def __init__(self):
+        a1 = 0
+        a2 = 1
+        a3 = 2
+
+    def sum(self, a1, a2, a3):
+        return a1+a2+a3
+
+
+b = ClassExample()
+print(b.sum(1, 2, 3))
