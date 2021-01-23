7a2b1462652f4adee45a5f23206b0fcdf82ac37e
SuperTapok <56495958+SuperTapok@users.noreply.github.com>
2021-01-23 23:53:09 +0700

    Update Neural_layer.py

--- Neural_layer.py
+++ Neural_layer.py
@@ -3,16 +3,14 @@ from Neuron import Neuron
 
 
 class NeuronLayer:
-    def __init__(self, previous_values, neurons):
-        self.__previous_layer = previous_values
+    def __init__(self, neurons):
         self._neurons = neurons
 
     def __calc(self, input_values, neurons):
         list = []
-        prev_values = np.array(input_values)
-        for i in range(neurons):
-            new_neuron = Neuron(prev_values)
-            list.append(new_neuron.get_result())
+        for i in range(self._neurons):
+            new_neuron = Neuron(self.previous_values)
+            list.append(new_neuron)
         return np.array(list)
 
     def get_result(self):
