b2d79ed4e4358183acb406bf62e568344a3f5f18
SuperTapok <56495958+SuperTapok@users.noreply.github.com>
2021-01-23 23:54:31 +0700

    Update Neural_net.py

--- Neural_net.py
+++ Neural_net.py
@@ -2,23 +2,22 @@ from Neural_layer import NeuronLayer
 
 
 class NeuralNet:
-    def __init__(self, input_values, numbers_of_layers, num_of_neurons_on_every_layer):
-        self.input_values = input_values
-        self.numbers_of_layers = numbers_of_layers
-        self.neurons = num_of_neurons_on_every_layer
+    def __init__(self, layer_numbers, neuron_numbers):
+        self.layer_numbers = layer_numbers
+        self.neurons = neuron_numbers
 
-    def __calc(self, input_values, quantity_of_neurons, number_of_layers):
+    def __calc(self, input_values):
         result = []
-        previous_values = input_values
-        for i in range(number_of_layers):
-            new_layer = NeuronLayer(previous_values, quantity_of_neurons)
-            previous_values = new_layer.get_result()
+        for i in range(self.layer_numbers):
+            new_layer = NeuronLayer(input_values, self.neurons)
+            input_values = new_layer.get_result()
         result = new_layer.get_result()
+        for i in result:
+            i = i.get_result()
         return result
 
-    def get_results(self):
-        return NeuralNet.__calc(self, self.input_values, self.neurons,  self.numbers_of_layers)
-
+    def get_results(self, input_values):
+        return NeuralNet.__calc(self, input_values)
 
 input_values = [0, 1, 0, 1]
 num_of_layers = 100
